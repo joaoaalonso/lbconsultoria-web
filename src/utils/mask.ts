c6db805601ab39301b4e8108133bf09762473467
João Alonso <joaoaalonso@gmail.com>
2024-10-18 15:08:02 -0300

    create masks

---
+++ src/utils/mask.ts
@@ -0,0 +1,9 @@
+export const PHONE_MASK = "(99) 99999-9999"
+
+export const CPF_ONLY_MASK = "999.999.999-99"
+
+export const CPF_MASK = "999.999.999-999"
+
+export const CNPJ_MASK = "99.999.999/9999-99"
+
+export const POSTAL_CODE_MASK = "99999-999"
